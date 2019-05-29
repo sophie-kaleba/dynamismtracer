4110c1ea04c71c15780b7c9a606748f01ae421bb
Aviral Goel <goel.aviral@gmail.com>
2019-05-29 14:33:42 -0400

    Add analysis report template and function to retrieve its path

--- R/analysis.R
+++ R/analysis.R
@@ -3,4 +3,10 @@ get_analysis_group <- function() {
     group_analyses(function_definitions_analysis,
                    dynamic_calls_analysis)
 }
+
+
+#' @export
+get_report_template <- function() {
+    create_report_template("report", "template.Rmd",
+                           package = "dynamismtracer")
 }
